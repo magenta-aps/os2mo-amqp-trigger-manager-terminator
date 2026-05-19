61bd509a45f2f15ccc0d868fb950b15a080183ec
Lorenzo Panieri <lp@magenta.dk>
2026-05-19 16:34:15 +0200

    refactor: re-wrote `_find_gaps` helper function to be easier to follow

--- manager_terminator/models.py
+++ manager_terminator/models.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 import datetime
+from enum import IntEnum
 from uuid import UUID
 
 from pydantic import BaseModel
@@ -10,3 +11,11 @@ class InvalidManagerPeriod(BaseModel):
     uuid: UUID
     from_: datetime.datetime
     to: datetime.datetime
+
+
+class ManagerState(IntEnum):
+    """Represents what state the manager is in at a specific point in time."""
+
+    NOT_SET = 0
+    VACANT = 1
+    SET = 2
